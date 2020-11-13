80d957c924e2236b56f75cff6e2fa2a880fc2d38
rimantepomeckyte <rimantepomeckyte@users.noreply.github.com>
2020-11-13 21:45:34 +0200

    Generator

--- js.js
+++ js.js
@@ -1,9 +1,9 @@
+//Bandymas su dviem
 const ivestasMokSk = document.querySelector("#ivestasMokSk");
 const ivestasUzdSk = document.querySelector("#ivestasUzdSk");
 const suzinokBtn = document.querySelector("#suzinokBtn");
 const tableBody = document.querySelector("#tableBody");
 const form = document.querySelector("#form");
-const table = document.querySelector("#table");
 const konteineris = document.getElementById("konteineris");
 
 suzinokBtn.addEventListener('click', pridekLentele);
@@ -17,6 +17,7 @@ function pridekLentele() {
     console.log("pradzia f-jos");
     const mokSk = ivestasMokSk.value;
     let uzdSk = ivestasUzdSk.value;
+    let skaicius = [];
 
     if (mokSk < 1 || uzdSk < 1) {
         alert("Blogai suvesti duomenys!");
@@ -33,7 +34,8 @@ function pridekLentele() {
             row.appendChild(cell1);
 
             const cell2 = document.createElement("td");
-            cell2.innerHTML = '<input type="text" placeholder="Vardas" class="text-center">';
+            cell2.innerHTML = '<input type="text" placeholder="Vardas">';
+            cell2.setAttribute("class", "text-center");
             row.appendChild(cell2);
 
         }
@@ -45,7 +47,7 @@ function pridekLentele() {
 const deleteTable = document.createElement("input");
 deleteTable.setAttribute("type", "button");
 deleteTable.setAttribute("value", "Ištrinti lentelę");
-deleteTable.setAttribute("class", "bg-info text-light");
+deleteTable.setAttribute("class", "bg-info text-light w-25 mb-4");
 konteineris.appendChild(deleteTable);
 deleteTable.addEventListener("click", deleteTBody);
 
@@ -54,6 +56,75 @@ function deleteTBody() {
     tableBody.innerHTML = "";
 }
 
+
+
+//Veikiantis su vienu mygtuku
+/*const ivestasMokSk = document.querySelector("#ivestasMokSk");
+//const ivestasUzdSk = document.querySelector("#ivestasUzdSk");
+const suzinokBtn = document.querySelector("#suzinokBtn");
+const tableBody = document.querySelector("#tableBody");
+const form = document.querySelector("#form");
+const konteineris = document.getElementById("konteineris");
+
+suzinokBtn.addEventListener('click', pridekLentele);
+console.log("pries f-ja");
+suzinokBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    form.reset();
+}, false);
+
+function pridekLentele() {
+    console.log("pradzia f-jos");
+    const mokSk = ivestasMokSk.value;
+    //let uzdSk = ivestasUzdSk.value;
+    let skaicius = [];
+
+    if (mokSk < 1) {
+        alert("Blogai suvesti duomenys!");
+    } else {
+
+        for (let i = 0; i < mokSk; i++) {
+            const row = document.createElement("tr");
+            tableBody.appendChild(row);
+
+            const cell1 = document.createElement("td");
+            let tinka = true;
+            while (tinka){
+                let rand = Math.floor(Math.random()*mokSk)+1;
+                if(skaicius.indexOf(rand) === -1){
+                    skaicius.push(rand);
+                    tinka=false;
+                }
+            }
+            cell1.innerHTML=skaicius[i];
+            cell1.setAttribute("class", "text-center");
+            row.appendChild(cell1);
+
+            const cell2 = document.createElement("td");
+            cell2.innerHTML = '<input type="text" placeholder="Vardas">';
+            cell2.setAttribute("class", "text-center");
+            row.appendChild(cell2);
+
+        }
+        console.log("pabaiga f-jos");
+    }
+}
+
+//istrynimo mygtukas
+const deleteTable = document.createElement("input");
+deleteTable.setAttribute("type", "button");
+deleteTable.setAttribute("value", "Ištrinti lentelę");
+deleteTable.setAttribute("class", "bg-info text-light w-25 mb-4");
+konteineris.appendChild(deleteTable);
+deleteTable.addEventListener("click", deleteTBody);
+
+function deleteTBody() {
+    console.log("delete");
+    tableBody.innerHTML = "";
+}*/
+
+
+//--------------------variantai ivairus----------------------------------
 /*        const masyvasRandom = [];
        for(let j=0; j<uzdSk; j++){
            const randomNumeris = Math.floor(Math.random() * uzdSk)+1;
@@ -82,3 +153,8 @@ function deleteTBody() {
         }
 
  */
+
+/*const randomNumeris = Math.floor(Math.random() * uzdSk)+1;
+            cell1.setAttribute("class", "text-center")
+            cell1.innerHTML = randomNumeris;
+row.appendChild(cell1);*/
